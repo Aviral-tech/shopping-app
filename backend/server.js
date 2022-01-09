19dd0cb135b4fa4cbb8fad56c26dd26c89415cda
Aviral-tech <nagpalavi2001@gmail.com>
2022-01-09 15:34:21 +0530

    added errorhandler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+import productRoutes from "./routes/productRoutes.js";
 
 const app = express();
 dotenv.config();
 
 connectDB();
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
+app.use(notFound);
+
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
